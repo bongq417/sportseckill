5ef2d6b7fa09843bc003e189a48b319f56413d0e
bongq417 <plw2010303382@163.com>
2021-11-23 09:34:15 +0800

    完善抢购逻辑，支持参数配置

--- sportseckill.go
+++ sportseckill.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-var offsetDay = flag.Duration("offsetDay", 5, "偏移天数0-5,默认5")
+var offsetDay = flag.Duration("offsetDay", 4, "偏移天数0-4,默认4")
 var start = flag.String("start", "", "开始时间，如：08:59:00")
 var runtime = flag.Duration("runtime", 60, "运行时长，默认60s")
 var hallTime = flag.String("hall", "18:00,20:00", "场次范围【13:00,14:00】、【20:00,22:00】")
